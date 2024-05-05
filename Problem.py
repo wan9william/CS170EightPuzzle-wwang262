faa96d818445f30670e0ebd9f6a5e6c056c55a4f
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2024-05-05 01:14:47 -0700

    added oop to problem class

--- Problem.py
+++ Problem.py
@@ -1,102 +1,88 @@
 import random
+import math
 
-board = [1, 2, 3, 4, 5, 6, 7, 8, 0]
-
-
-def my_function():
-    print("Hello World!")
-
-
-def print_board():
-    for x in range(0, 3):
-        print(board[3 * x], board[3 * x + 1], board[3 * x + 2])
-    print("\n")
-
-
-def move_left():
-    x = board.index(0)
-    if x % 3 != 0:
-        temp = board[x - 1]
-        board[x - 1] = board[x]
-        board[x] = temp
-
-
-def move_right():
-    x = board.index(0)
-    if x % 3 != 2:
-        temp = board[x + 1]
-        board[x + 1] = board[x]
-        board[x] = temp
-
-
-def move_up():
-    x = board.index(0)
-    if x / 3 > 1:
-        temp = board[x - 3]
-        board[x - 3] = board[x]
-        board[x] = temp
-
-
-def move_down():
-    x = board.index(0)
-    if x / 3 < 2:
-        temp = board[x + 3]
-        board[x + 3] = board[x]
-        board[x] = temp
-
-
-def scramble_board():
-    for x in range(0, random.randint(0, 1000)):
-        temp_state = random.randint(0, 3)
-        match temp_state:
-            case 0:
-                move_left()
-            case 1:
-                move_right()
-            case 2:
-                move_down()
-            case 3:
-                move_up()
+class Problem:
+    def __init__(self,initialState):
+        self.initialState = initialState.copy()
+        self.currentState = initialState.copy()
+        self.size = int(math.sqrt(len(self.initialState)))
+    def left(self):
+        x = self.currentState.index(0)
+        if x % self.size != 0:
+            temp = self.currentState[x - 1]
+            self.currentState[x - 1] = self.currentState[x]
+            self.currentState[x] = temp
+    def right(self):
+        x = self.currentState.index(0)
+        if x % self.size != self.size-1:
+            temp = self.currentState[x + 1]
+            self.currentState[x + 1] = self.currentState[x]
+            self.currentState[x] = temp
+    def up(self):
+        x = self.currentState.index(0)
+        if x / self.size >= 1:
+            temp = self.currentState[x - self.size]
+            self.currentState[x - self.size] = self.currentState[x]
+            self.currentState[x] = temp
+    def down(self):
+        x = self.currentState.index(0)
+        if x / self.size < self.size:
+            temp = self.currentState[x + self.size]
+            self.currentState[x + self.size] = self.currentState[x]
+            self.currentState[x] = temp
+    def scramble(self):
+        for x in range(0, random.randint(0, 1000)):
+            temp_state = random.randint(0, 3)
+            match temp_state:
+                case 0:
+                    self.left()
+                case 1:
+                    self.right()
+                case 2:
+                    self.up()
+                case 3:
+                    self.down()
+    def check_solution(self):
+        sizeSquared = self.size * self.size
+        for x in range(0, sizeSquared):
+            if (x != sizeSquared-1 and self.currentState[x] != x+1) or (x == sizeSquared-1 and self.currentState[x] != 0):
+                return False
+        return True
+    def print_curr_state(self):
+        a = self.currentState
+        b = self.size
+        for x in range(0, self.size*self.size,self.size):
+            print(self.currentState[x:x+b])
+        # print("\n")
+        
 
 
-def check_solution():
-    for x in range(0, 9):
-        print(x+1,":",board[x])
-        if (board[x] != x+1) & (x == 9 & board[x] != 0):
-            return False
-    return True
+
+def createInitState(size):
+    return [x if x != size*size else 0 for x in range (1,size*size+1)]
 
 
-print_board()
-print(check_solution())
-move_left()
-print_board()
-print(check_solution())
-move_left()
-print_board()
-move_left()
-print_board()
-move_right()
-print_board()
-move_right()
-print_board()
-move_right()
-print_board()
-move_up()
-print_board()
-move_up()
-print_board()
-move_up()
-print_board()
-move_down()
-print_board()
-move_down()
-print_board()
-move_down()
-print_board()
-move_up()
-print_board()
-move_left()
-print_board()
-scramble_board()
-print_board()
+
+
+
+
+
+
+
+p = Problem(createInitState(4))
+# print(p.initialState)
+print(p.check_solution())
+p.left()
+# p.left()
+p.up()
+p.up()
+p.up()
+p.up()
+p.up()
+p.up()
+# p.up()
+# print(p.check_solution())
+p.print_curr_state()
+print(p.size)
+
+# print([x if x != 9 else 0 for x in range (1,10)])
