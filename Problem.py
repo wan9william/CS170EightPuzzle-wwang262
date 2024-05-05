84f71f32f76bfe2a67417b1fb6029afd21e5855d
Manzana <73004527+ManzanaNaranja@users.noreply.github.com>
2024-05-05 03:06:41 -0700

    got search function to solve it

--- Problem.py
+++ Problem.py
@@ -6,50 +6,68 @@ class Problem:
         self.initialState = initialState.copy()
         self.currentState = initialState.copy()
         self.size = int(math.sqrt(len(self.initialState)))
-    def left(self):
-        x = self.currentState.index(0)
+    def left(self,state):
+        x = state.index(0)
         if x % self.size != 0:
-            temp = self.currentState[x - 1]
-            self.currentState[x - 1] = self.currentState[x]
-            self.currentState[x] = temp
-    def right(self):
-        x = self.currentState.index(0)
+            temp = state[x - 1]
+            state[x - 1] = state[x]
+            state[x] = temp
+            return True
+        else:
+            return False
+    def right(self,state):
+        x = state.index(0)
         if x % self.size != self.size-1:
-            temp = self.currentState[x + 1]
-            self.currentState[x + 1] = self.currentState[x]
-            self.currentState[x] = temp
-    def up(self):
-        x = self.currentState.index(0)
+            temp = state[x + 1]
+            state[x + 1] = state[x]
+            state[x] = temp
+            return True
+        else:
+            return False
+    def up(self,state):
+        x = state.index(0)
         if x / self.size >= 1:
-            temp = self.currentState[x - self.size]
-            self.currentState[x - self.size] = self.currentState[x]
-            self.currentState[x] = temp
-    def down(self):
-        x = self.currentState.index(0)
-        if x / self.size < self.size:
-            temp = self.currentState[x + self.size]
-            self.currentState[x + self.size] = self.currentState[x]
-            self.currentState[x] = temp
-    def scramble(self):
+            temp = state[x - self.size]
+            state[x - self.size] = state[x]
+            state[x] = temp
+            return True
+        else:
+            return False
+    def down(self,state):
+        x = state.index(0)
+        if x + self.size < self.size*self.size-1:
+            temp = state[x + self.size]
+            state[x + self.size] = state[x]
+            state[x] = temp
+            return True
+        else:
+            return False
+    def scramble(self,state):
         for x in range(0, random.randint(0, 1000)):
             temp_state = random.randint(0, 3)
             match temp_state:
                 case 0:
-                    self.left()
+                    self.left(state)
                 case 1:
-                    self.right()
+                    self.right(state)
                 case 2:
-                    self.up()
+                    self.up(state)
                 case 3:
-                    self.down()
+                    self.down(state)
     def check_solution(self):
         sizeSquared = self.size * self.size
         for x in range(0, sizeSquared):
             if (x != sizeSquared-1 and self.currentState[x] != x+1) or (x == sizeSquared-1 and self.currentState[x] != 0):
                 return False
         return True
-    def print_curr_state(self):
-        a = self.currentState
+    def check_solution(self,state):
+        sizeSquared = self.size * self.size
+        for x in range(0, sizeSquared):
+            if (x != sizeSquared-1 and state[x] != x+1) or (x == sizeSquared-1 and state[x] != 0):
+                return False
+        return True
+    def print_state(self,state):
+        a = state
         b = self.size
         for x in range(0, self.size*self.size,self.size):
             print(self.currentState[x:x+b])
@@ -69,20 +87,16 @@ def createInitState(size):
 
 
 
-p = Problem(createInitState(4))
+# p = Problem(createInitState(3))
+# # print(p.initialState)
 # print(p.initialState)
-print(p.check_solution())
-p.left()
+# a = p.initialState.copy()
+# p.left(a)
+# p.print_state(a)
 # p.left()
-p.up()
-p.up()
-p.up()
-p.up()
-p.up()
-p.up()
 # p.up()
-# print(p.check_solution())
-p.print_curr_state()
-print(p.size)
+# p.up()
+# p.up()
+# p.up()
+# p.up()
 
-# print([x if x != 9 else 0 for x in range (1,10)])
--- stuff.py
+++ stuff.py
@@ -1,37 +1,52 @@
 import heapq as hp
+import Problem
 
+exploredStates = dict()
 
-def in_goal_state(node):
-    count = 1
-    size = len(node)
-    for i in range(0,size):
-        for j in range(0,size):
-            if node[i][j] != count and i != size-1 and j != size-1:
-                return False
-            elif i == size-1 and j == size-1 and node[i][j] != 0:
-                return False
-            count += 1
-    return True
+def expand(problem, nodething):
+    node = nodething[1]
+    a = []
+    left = node.copy()
+    right = node.copy()
+    up = node.copy()
+    down = node.copy()
+    if problem.left(left):
+        a.append((1,left))
+    if problem.right(right):
+        a.append((1,right))
+    if problem.up(up):
+        a.append((1,up))
+    if problem.down(down):
+        a.append((1,down))
+    return a
+
 
 def queueing_function(heap, nodes):
-    pass
-
-def expand(node):
-    pass
-
+    for i in range(0, len(nodes)):
+        node = nodes[i]
+        hp.heappush(heap, node)
 
 def general_search(problem):
     nodes = []
-    hp.heappush(nodes, problem)
+    hp.heappush(nodes, (1,problem.initialState))
     while True:
         if len(nodes) <= 0:
             return -1 # failure
-        node = hp.heappop(nodes)       
-        if in_goal_state(node):
+        node = hp.heappop(nodes)   
+        # print(node)    
+        # print(problem.check_solution(node[1]))
+        print(node)
+        print(problem.check_solution(node[1]))
+        if problem.check_solution(node[1]):
             return node
-        nodes = queueing_function(nodes, expand(node))
+        if str(node[1]) not in exploredStates:
+            exploredStates[str(node[1])] = True
+            queueing_function(nodes, expand(problem, node))
         
 
+p = Problem.Problem(Problem.createInitState(3))
+state = p.initialState
+p.scramble(state)
 
-general_search([[1,2,3], [4,5,6], [7,8,0]])
-print(in_goal_state([[1,2,3], [4,5,6], [7,8,0]]))
+general_search(p)
+# print(str(state))
